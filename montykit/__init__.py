39592a4caeda713f832cbe1c4a726681dadb0692
tyleruploads <tyleruploads@yahoo.com>
2026-01-15 18:10:02 -0500

    Use mkinit to make init

--- montykit/__init__.py
+++ montykit/__init__.py
@@ -1,1 +1,39 @@
 __version__ = "0.1.0"
+
+from . import analysis
+from . import ciphers
+from . import converters
+from . import generator
+from . import hash
+from . import validators
+
+from .analysis import (detect_lang, text_difficulty, text_is_difficult,
+                       text_polarity, text_subjectivity, word_freq,)
+from .ciphers import (a1z26_cipher, atbash_cipher, bacon_cipher, caesar_cipher,
+                      eng_to_imct, eng_to_morse, morse_to_eng,
+                      rail_fence_2_cipher, reverse_cipher, rot13, shift_cipher,
+                      substitution_cipher,)
+from .converters import (base64_decode, base64_encode, binary_to_text,
+                         hex_to_text, text_to_binary, text_to_hex, text_to_url,
+                         to_camel_case, to_snake_case, url_to_text,)
+from .generator import (gen_first_name, gen_first_names, gen_full_name,
+                        gen_full_names, gen_id, gen_last_name, gen_last_names,
+                        gen_middle_name, gen_middle_names, gen_password,
+                        gen_phone, gen_uuid,)
+from .hash import (generate_hash,)
+from .validators import (is_email, is_strong_pass, json_validator,)
+
+__all__ = ['a1z26_cipher', 'analysis', 'atbash_cipher', 'bacon_cipher',
+           'base64_decode', 'base64_encode', 'binary_to_text', 'caesar_cipher',
+           'ciphers', 'converters', 'detect_lang', 'eng_to_imct',
+           'eng_to_morse', 'gen_first_name', 'gen_first_names',
+           'gen_full_name', 'gen_full_names', 'gen_id', 'gen_last_name',
+           'gen_last_names', 'gen_middle_name', 'gen_middle_names',
+           'gen_password', 'gen_phone', 'gen_uuid', 'generate_hash',
+           'generator', 'hash', 'hex_to_text', 'is_email', 'is_strong_pass',
+           'json_validator', 'morse_to_eng', 'rail_fence_2_cipher',
+           'reverse_cipher', 'rot13', 'shift_cipher', 'substitution_cipher',
+           'text_difficulty', 'text_is_difficult', 'text_polarity',
+           'text_subjectivity', 'text_to_binary', 'text_to_hex', 'text_to_url',
+           'to_camel_case', 'to_snake_case', 'url_to_text', 'validators',
+           'word_freq']
